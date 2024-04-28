66d5f901efa3e7ebd19ab8cb9d08da395cb7c5f1
Carlos Vital <carloshenrique.vital@gmail.com>
2024-04-28 11:45:16 -0300

    add ssl config

--- infra/database.js
+++ infra/database.js
@@ -11,7 +11,7 @@ async function query(queryObject) {
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
@@ -22,6 +22,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production"
   });
 
   await client.connect();
